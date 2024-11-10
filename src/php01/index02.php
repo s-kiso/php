69c7069c0c222e5ca6f47ac2b3937002ae245435
soto <kiso.soto@gmail.com>
2024-11-11 08:08:28 +0900

    php05の追加

---
+++ src/php01/index02.php
@@ -0,0 +1,15 @@
+<?php
+$item = "JavaScript";
+echo $item;
+
+const FELANGUAGE = "ja";
+const FELANGUAGE = "en"; //これは定数だから上書きされない
+echo FELANGUAGE;
+
+$name = "Tanaka";
+$last_name = "Yamada";
+$first_name = "Saburo";
+
+echo $name."Jiro";
+echo "<br />";
+echo $last_name . $first_name;
---
+++ src/php01/index03.php
@@ -0,0 +1,27 @@
+<?php
+
+$a = 15;
+$b = 3;
+$c = 10;
+
+echo $a + $b;
+echo "<br />";
+echo $a - $b;
+
+$a =15;
+$b = 3;
+$c = 10;
+$d = 5;
+
+$c +=$d;
+echo $c; 
+
+$a = 20;
+$b = 5;
+
+echo "<br />";
+echo ($a < $b);
+echo "<br />";
+echo ($a > 10 && $a < 30);
+echo "<br />";
+
